f17268ed7541004c9060ad5d7e55fb0d9945fd5e
Alfonso Tienda <atienda@iprocuratio.com>
2018-12-06 15:49:16 +0100

    web corporativa

--- webpersonal/portfolio/models.py
+++ webpersonal/portfolio/models.py
@@ -7,7 +7,7 @@ class Project(models.Model):
     image = models.ImageField(verbose_name="Imagen", upload_to="projects")
     created = models.DateTimeField(auto_now_add=True, verbose_name="Fecha de creación")
     updated = models.DateTimeField(auto_now=True, verbose_name="Fecha de actualización")
-
+    urlfield = models.URLField(null=True, blank=True, max_length=200, verbose_name="Dirección Web")
     class Meta:
         verbose_name = "proyecto"
         verbose_name_plural = "proyectos"
