4c72d2fa7c9672a0d7fc80c679b8ad47ea4df810
Alfonso Tienda <atienda@iprocuratio.com>
2018-12-06 17:01:32 +0100

    empresa v1

---
+++ webempresa/core/urls.py
@@ -0,0 +1,12 @@
+from django.urls import path
+from . import views
+
+urlpatterns = [
+    path('', views.home),
+    path('about/', views.about),
+    path('services/', views.services),
+    path('store/', views.store),
+    path('contact/', views.contact),
+    path('blog/', views.blog),
+    path('sample/', views.sample)
+    ]
