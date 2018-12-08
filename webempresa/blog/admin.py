d35794a37ceaeb203bd79253565f3f199b33e80c
Alfonso Tienda <atienda@iprocuratio.com>
2018-12-08 17:41:16 +0100

    blog termnado

--- webempresa/blog/admin.py
+++ webempresa/blog/admin.py
@@ -1,3 +1,15 @@
 from django.contrib import admin
-
+from .models import Category, Post
 # Register your models here.
+@admin.register(Category)
+class CategoryAdmin(admin.ModelAdmin):
+    readonly_fields = ('created', 'updated')
+
+@admin.register(Post)
+class PostAdmin(admin.ModelAdmin):
+    readonly_fields = ('created', 'updated')
+
+
+
+
+
--- webempresa/blog/models.py
+++ webempresa/blog/models.py
@@ -1,17 +1,43 @@
 from django.db import models
+from django.contrib.auth.models import User
+from django.utils.timezone import now
+
 
 # Create your models here.
 class Category(models.Model):
-    name = models.CharField(length = 50)
+    name = models.CharField(max_length = 100)
+    created = models.DateTimeField(auto_now_add=True, verbose_name="Fecha de creación")
+    updated = models.DateTimeField(auto_now=True, verbose_name="Fecha de actualización")
     
-
     class Meta:
-        verbose_name = _("Categoría")
-        verbose_name_plural = _("Categorías")
-        ordering = '-created'
+        verbose_name = "Categoría"
+        verbose_name_plural = "Categorías"
+        ordering = ['-created']
 
     def __str__(self):
         return self.name
 
     def get_absolute_url(self):
         return reverse("Category_detail", kwargs={"pk": self.pk})
+
+class Post(models.Model):
+    title = models.CharField( max_length=200, verbose_name="Título")
+    content = models.TextField(verbose_name="Contenido")
+    published = models.DateTimeField(auto_now=False, auto_now_add=False, verbose_name="Fecha de publicación", default=now())
+    image = models.ImageField(upload_to="post", verbose_name="Imagen",   null=True , blank="True")
+    author = models.ForeignKey(User, verbose_name="Autor", on_delete=models.CASCADE)
+    categories = models.ManyToManyField(Category, verbose_name="Categorías")
+    created = models.DateTimeField(auto_now_add=True, verbose_name="Fecha de creación")
+    updated = models.DateTimeField(auto_now=True, verbose_name="Fecha de actualización")
+
+    class Meta:
+        verbose_name = "entrada"
+        verbose_name_plural = "entradas"
+        ordering = ['-created']
+
+    def __str__(self):
+        return self.title
+
+    def get_absolute_url(self):
+        return reverse("Post_detail", kwargs={"pk": self.pk})
+
