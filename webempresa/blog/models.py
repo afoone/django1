24ef47c53a3a9b021bf2e8f46ad85c51daeaa2e5
Alfonso Tienda <atienda@iprocuratio.com>
2018-12-08 17:00:25 +0100

    models for blog

--- webempresa/blog/models.py
+++ webempresa/blog/models.py
@@ -1,3 +1,17 @@
 from django.db import models
 
 # Create your models here.
+class Category(models.Model):
+    name = models.CharField(length = 50)
+    
+
+    class Meta:
+        verbose_name = _("Categoría")
+        verbose_name_plural = _("Categorías")
+        ordering = '-created'
+
+    def __str__(self):
+        return self.name
+
+    def get_absolute_url(self):
+        return reverse("Category_detail", kwargs={"pk": self.pk})
