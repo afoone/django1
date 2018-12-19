61ce908b33228af98e13446f0150ce26e5db2e48
Alfonso <afoone@hotmail.com>
2018-12-19 15:28:19 +0100

    create

--- webplayground/pages/urls.py
+++ webplayground/pages/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
-from .views import PageListView, PageDetailView
+from .views import PageListView, PageDetailView, PageCreateView
 
-urlpatterns = [
+pages_patters = ([
     path('', PageListView.as_view(), name='pages'),
     path('<int:pk>/<slug:page_slug>/', PageDetailView.as_view(), name='page'),
-]
+    path('create', PageCreateView.as_view(), name='create'),
+], 'pages')
--- webplayground/pages/views.py
+++ webplayground/pages/views.py
@@ -1,6 +1,9 @@
 from django.views.generic.list import ListView
 from django.views.generic.detail import DetailView
+from django.views.generic.edit import CreateView
 from .models import Page
+from django.urls import reverse
+
 
 # Create your views here.
 class PageListView(ListView):
@@ -8,4 +11,10 @@ class PageListView(ListView):
 
 class PageDetailView(DetailView):
     model = Page
+
+class PageCreateView(CreateView):
+    model = Page
+    fields = ['title', 'content', 'order']
+    def get_success_url(self):
+        return reverse('pages:pages')
 
